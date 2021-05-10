43c13567d41bde0e788c50f809ba676d35dd239e
Aadarshkashyap51 <aadarshkashyap51@gmail.com>
2021-05-10 21:28:28 +0530

    Header Work  in process

--- src/components/login.js
+++ src/components/login.js
@@ -6,15 +6,54 @@ const Login = (props) => {
         <Container>
             <Nav>
                 <a href="/" alt="">
-                    <img src="/image/login-login.svg" alt="" />
+                    <img src="/images/login-logo.svg" alt="" />
                 </a>
+                <div>
+                    <Join>Join Now</Join>
+                </div>
             </Nav>
         </Container>
     )
 };
 
-const Container = styled.div``;
+const Container = styled.div`
+    padding: 0px;
 
-const Nav = styled.nav``;
+`;
+
+const Nav = styled.nav`
+    max-width: 1128px;
+    margin: auto;
+    padding: 12px 0 16px;
+    display: flex;
+    align-items: center;
+    position: relative;
+    justify-content: space-between;
+    flex-wrap: nowrap;
+
+    & > a{
+        width: 135px;
+        height: 34px;
+        @media (max-width: 768px) {
+            padding: 0 5px;
+        }
+    }
+
+
+`;
+
+const Join = styled.a`
+  font-size: 16px;
+  padding: 10px 12px;
+  text-decoration: none;
+  border-radius: 4px;
+  color: rgba(0, 0, 0, 0.6);
+  margin-right: 12px;
+  &:hover {
+    background-color: rgba(0, 0, 0, 0.08);
+    color: rgba(0, 0, 0, 0.9);
+    text-decoration: none;
+  }
+`;
 
 export default Login;
