0ebcadf83138e4da904b61b6fafc4e1e18cc88bf
Aadarshkashyap51 <aadarshkashyap51@gmail.com>
2021-05-10 20:59:58 +0530

    login changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
+import Login from "./components/Login";
 
 function App() {
   return (
     <div className="App">
-      Lets Start 🚀
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Login />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/components/login.js
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+const Login = (props) => {
+
+    return (
+        <Container>
+            <Nav>
+                <a href="/" alt="">
+                    <img src="/image/login-login.svg" alt="" />
+                </a>
+            </Nav>
+        </Container>
+    )
+};
+
+const Container = styled.div``;
+
+const Nav = styled.nav``;
+
+export default Login;
